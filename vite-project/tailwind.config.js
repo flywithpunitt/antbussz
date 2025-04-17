0e9930fe10af5581720145df0128dfd8d38ebdf2
flywithpunitt <punitkumar9168@gmail.com>
2025-04-17 18:40:21 +0530

    Initial commit: ANT Bus booking website

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'slide': 'slide 20s linear infinite',
+        'scroll': 'scroll 20s linear infinite'
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' }
+        }
+      }
+    },
+  },
+  plugins: [],
+} 
